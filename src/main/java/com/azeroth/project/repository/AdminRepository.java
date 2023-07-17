9fac67ff64260f22681be17dfc16a6cdd4e4c2c5
IamHongSeok <hongseok890507@gmail.com>
2023-07-17 21:38:08 +0900

    #HSKim : 1.테스트 DATA INSERT 쿼리문 작성 2.ADMIN Repository 및 메퍼정리.

--- src/main/java/com/azeroth/project/repository/AdminRepository.java
+++ src/main/java/com/azeroth/project/repository/AdminRepository.java
@@ -1,18 +1,29 @@
 package com.azeroth.project.repository;
 
-import com.azeroth.project.domain.CardDomain;
+import com.azeroth.project.domain.*;
+
+import java.util.List;
 
 public interface AdminRepository {
 
     // 카드 조회
     CardDomain findCard(CardDomain card);
     // 카드 결재
-    int update(CardDomain card);
+    int updateCard(CardDomain card);
 
     // 회원 목록 조회
+    List<UserDomain> ListByUsers();
+
     // 특정 회원 검색
+    UserDomain findByUser(String username);
+
     // 특정 회원이 구매한 상품 list
+    List<ProductDomain> userBuyProduct(String username);
+
     // 특정 회원이 작성한 후기 list
+    List<ReviewDomain> writeReviewByUser(String username);
+
     // 답변이 이루어지지 않은 후기 list
+    List<ReviewDomain> NoAnswerReview();
 
 }
--- src/main/java/com/azeroth/project/service/AdminService.java
+++ src/main/java/com/azeroth/project/service/AdminService.java
@@ -1,6 +1,8 @@
 package com.azeroth.project.service;
 
 import com.azeroth.project.domain.AuthorityDomain;
+import com.azeroth.project.domain.ReviewDomain;
+import com.azeroth.project.domain.SalesDomain;
 import com.azeroth.project.domain.UserDomain;
 import org.apache.catalina.User;
 
@@ -8,16 +10,30 @@ import java.util.List;
 
 public interface AdminService {
     // Spring Security 처리
+
     // 로그인 처리
-    // 고객 관리 기능
+
+    // 고객 관리 기능 ( 권한 설정등 )
+    UserDomain UpdateUserInfo();
+
+
     // 회원 LIST ( 가입된 일반 사용자 LIST 출력)
+    List<UserDomain> findByAllUser();
+
     // 회원 검색 ( ID / 닉네임 / EMAIL 등)
+    UserDomain findByUsername(String username);
+
     // 특정 회원이 구매한 상품 LIST
+    List<SalesDomain> selectBuyByUser(String username);
+
     // 특정 회원이 작성한 후기 LIST
+    List<ReviewDomain> selectAnswerByUser(String username);
+
     // 답변 미 작성 된 후기 LIST ( 답변을 달 수 있도록 처리)
+    List<ReviewDomain> selectNoAnswerReview();
+
     // 사용자 권한 조회
     List<AuthorityDomain>  selectAuthoritiesById(Long id);
 
-    UserDomain findByUsername(String username);
 
 }
