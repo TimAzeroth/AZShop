710bd60d65190259e6701ddf9301b9d0e33ecf46
IamHongSeok <hongseok890507@gmail.com>
2023-07-17 18:42:11 +0900

    #HSKim : 시큐어처리 임시완료

--- src/main/java/com/azeroth/project/service/ReviewServiceImpl.java
+++ src/main/java/com/azeroth/project/service/ReviewServiceImpl.java
@@ -2,9 +2,11 @@ package com.azeroth.project.service;
 
 import com.azeroth.project.domain.ReviewDomain;
 import com.azeroth.project.repository.ReviewRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ReviewServiceImpl implements ReviewService{
 
     private ReviewRepository reviewRepository;
