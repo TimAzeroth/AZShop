5559ce07db7067e49c4bf454db05b56a60b0b365
KiDoYe <poi6857@gmail.com>
2023-07-17 14:18:40 +0900

    sales.html 수정, SalesController, SalesService, SalesServiceImpl 수정

--- src/main/java/com/azeroth/project/service/SalesServiceImpl.java
+++ src/main/java/com/azeroth/project/service/SalesServiceImpl.java
@@ -1,14 +1,25 @@
 package com.azeroth.project.service;
 
 import com.azeroth.project.domain.SalesDomain;
+import com.azeroth.project.repository.SalesRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class SalesServiceImpl implements SalesService {
+public abstract class SalesServiceImpl implements SalesService {
+
+    private SalesRepository salesRepository;
+
+    @Override
+    public SalesDomain sales(SalesDomain salesDomain) {
+        return null;
+    }
+
     @Override
     public List<SalesDomain> saleslist() {
         return null;
     }
+
+
 }
