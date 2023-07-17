d60486962c218b7d9d98d6fe9d250bbcebab9529
KiDoYe <poi6857@gmail.com>
2023-07-17 21:47:41 +0900

    sales.html 수정

---
+++ ERD/projectTest.sql
@@ -0,0 +1,47 @@
+
+
+select username, email, phone from user
+-- where username = ""
+;
+
+
+select 
+	u.username,
+	u.nickname,
+	u.email,
+	u.phone,
+	a.address_detail,
+	a.address,
+	a.postcode,
+	p.id
+from 
+	address a, 
+	`user` u,
+	product p ;
+
+
+
+SELECT id, authority_id, username, password, 
+nickname, email, profileimg, u_status, phone, 
+logdate, regdate
+FROM azerothdb.`user`;
+
+
+SELECT id, user_id, address_name, address, address_detail, postcode
+FROM azerothdb.address;
+
+
+SELECT id, p_name, main_cate, sub_cate, p_img, detail, price, stock, p_rank
+FROM azerothdb.product;
+
+
+SELECT id, u_username, p_id, amount, address, address_detail, postcode, deliveryreq, tracknum
+FROM azerothdb.sales;
+
+
+
+
+
+
+
+
