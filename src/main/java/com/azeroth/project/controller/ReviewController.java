1e4ba760df68fe5a5f1865df3ba7f87dd82128b8
kotaehyun <rhxocm@gmail.com>
2023-07-18 20:10:51 +0900

    라뷰(html/Controller/Repository/xml/ServicelImpl) 변경되었습니다.

--- src/main/java/com/azeroth/project/controller/ReviewController.java
+++ src/main/java/com/azeroth/project/controller/ReviewController.java
@@ -2,35 +2,38 @@ package com.azeroth.project.controller;
 
 
 import com.azeroth.project.domain.ReviewDomain;
+import com.azeroth.project.repository.ReviewRepository;
 import com.azeroth.project.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
 
-@RestController
+@Controller
 @RequestMapping("/review")
 public class ReviewController {
-
-    private final ReviewService reviewService;
-
     @Autowired
-    public ReviewController(ReviewService reviewService) {
-        this.reviewService = reviewService;
+    private ReviewService reviewService;
+
+    public ReviewController() {
+        System.out.println(getClass().getName() + "()생성");
     }
 
-    @PostMapping
-    public ReviewDomain createReview(ReviewDomain reviewDomain){
-        return reviewService.saveReview(reviewDomain);
-    }
+
+//    @PostMapping()
+//    public ReviewDomain createReview(ReviewDomain reviewDomain){
+//
+//        return reviewService.saveReview(reviewDomain);
+//    }
 
     @GetMapping
-    public List<ReviewDomain> getAllReviews(){
-        return reviewService.getAllReviews();
+    public String reviews(){
+        return "review/review";
     }
 
 
--- src/main/java/com/azeroth/project/repository/ReviewRepository.java
+++ src/main/java/com/azeroth/project/repository/ReviewRepository.java
@@ -13,4 +13,10 @@ public interface ReviewRepository {
     // 리뷰 수정
     int update(ReviewDomain review);
 
+    // 리뷰 글을 관리자가 답변
+    int addReply(ReviewDomain review);
+
+    // 관리자 답변글 표시
+    int makeVisible(long id);
+
 }
