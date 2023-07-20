76286d714119fc79ecc1fe206e774ef5e1f26182
HSKim <hongseok8905@naver.com>
2023-07-21 00:28:07 +0900

    #HSKim : 통합과정중. 레파지터리.xml 수정

--- src/main/java/com/azeroth/project/service/AdminServiceImpl.java
+++ src/main/java/com/azeroth/project/service/AdminServiceImpl.java
@@ -2,51 +2,46 @@ package com.azeroth.project.service;
 
 import com.azeroth.project.domain.*;
 import com.azeroth.project.repository.AdminRepository;
-import com.azeroth.project.repository.CategoryRepository;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import lombok.ToString;
+import com.azeroth.project.domain.SalesChkDomain;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
+
 
 @Service
 public class AdminServiceImpl implements AdminService {
 
     private AdminRepository adminRepository;
+    private SalesChkDomain schk;
 
     @Autowired
     public AdminServiceImpl(SqlSession sqlSession) {
+
         adminRepository = sqlSession.getMapper(AdminRepository.class);
     }
 
     @Override
     public UserDomain UpdateUserInfo() {
+
         return null;
     }
 
     @Override
     public List<UserDomain> findByAllUser() {
+
         return adminRepository.ListByUsers();
     }
 
     @Override
     public UserDomain findByUsername(String username) {
+
         return adminRepository.findByUser(username);
     }
 
     @Override
     public List<SalesDomain> selectBuyByUser(String username) {
-
-
         return null;
     }
 
@@ -66,7 +61,7 @@ public class AdminServiceImpl implements AdminService {
     }
 
     public SalesChkDomain salesCHK (CardDomain card){
-        SalesChkDomain schk = new SalesChkDomain();
+
         CardDomain payCard = adminRepository.findCard(card);
         schk.setChkProcess(false);
         schk.setEreMag("정상 처리되었습니다.");
