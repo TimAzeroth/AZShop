d6b70825ff7646f8fd8a22fe5558453ffe15527f
KiDoYe <poi6857@gmail.com>
2023-07-20 21:46:35 +0900

    Sales html, Controller, Service, Repository 쿼리문 수정 sales.js 생성 및 내용 작성

--- src/main/java/com/azeroth/project/service/SalesServiceImpl.java
+++ src/main/java/com/azeroth/project/service/SalesServiceImpl.java
@@ -29,13 +29,28 @@ public class SalesServiceImpl implements SalesService {
 
     // 주문 / 결제 화면에 정보 불러오기
     @Override
-    public SalesDomain saleslist(String u_username) {
-        SalesDomain salesDomain = salesRepository.findSales(u_username);
+    public SalesDomain saleslist(String username) {
+        SalesDomain salesDomain = salesRepository.findSales(username);
         return salesDomain;
     }
 
     @Override
     public int insert(SalesDomain salesDomain) {
+        // session정보 땜빵
+        UserDomain user = null;
+
+        user.setId(1L);
+        user.setAuthority_id(1L);
+        user.setUsername("user1");
+        user.setNickname("사용자1");
+        user.setEmail("user1@gmail.com");
+        user.setProfileimg("face01.png");
+        user.setU_status("USE");
+        user.setPhone("01012341234");
+
+        // 위 정보는 session 의 정보,  DB 에서 다시 읽어온다
+        user = userRepository.findByUsername(user.getUsername());
+        salesDomain.setUser(user);
 
         // UserDomain userDomain = (유틸).getLoggedUser();
 
