7ce594b8088b51ef7965c8be29d2428dad548c6a
BuildTools <unconfigured@null.spigotmc.org>
2023-07-31 10:51:15 +0900

    - 가격 표시 오류 수정

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -1,14 +1,11 @@
 $(function() {
-    let total = 0;
-    $('.price').each(function() {
-        total += parseInt($(this).text());
-    });
-    $('#total').text(total);
-    $('#totalinput').val(total);
+    refreshTotal();
 
     $('.productAmount').on('change', function() {
         const productId = $(this).attr('id').replace('amountSelect-', '');
         const amount = $(`#amountSelect-${productId}`).val();
+        const singlePrice = parseInt($(`#singlePrice-${productId}`).val());
+        changeValues(productId, singlePrice, amount);
 
         const data = {
             "product_id": productId,
@@ -29,3 +26,18 @@ $(function() {
 
     });
 });
+
+function changeValues(productId, singlePrice, amount) {
+    var priceT = singlePrice * amount;
+    $(`#price-${productId}`).text(priceT);
+    refreshTotal();
+}
+
+function refreshTotal() {
+    let total = 0;
+    $('.price').each(function() {
+        total += parseInt($(this).text());
+    });
+    $('#total').text(total);
+    $('#totalinput').val(total);
+}
